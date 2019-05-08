c72cea382fd20701f484a0ec455e783f00bdf723
gol.d.eleacid <ele.leader@gmail.com>
2019-05-08 13:32:33 -0500

    forma simplificada del anterior commit

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     {
       //implementacion basica para obtener el id de otra tabla
       //ejecutamos una consulta y guardamos el resultado en una variable
-      $area = DB::table('area')->select('id')->take(1)->get();
+      $area = DB::table('area')->select('id')->first();//forma mas corta de capturar el primer elemento del objeto
 
       dd($area);
 
@@ -23,7 +23,7 @@ class UserSeeder extends Seeder
           'email'     =>  'ele.leader@gmail.com',
           'nivel'     =>  'admin',
           'password'  =>  bcrypt('LseRdlP0P'),
-          'area_id'   =>  $area[0]->id//regresa un objeto
+          'area_id'   =>  $area->id//ahora solo asignamos la variable
         ]);
     }
 }
