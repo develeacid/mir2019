f1e91a06d8b4e410af8f7d1e0b4992d9ef34c53a
gol.d.eleacid <ele.leader@gmail.com>
2019-05-08 13:29:30 -0500

    resuelto problema con las llaves foraneas, implementacion de una consulta para generar un id valido

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,11 +12,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+      //implementacion basica para obtener el id de otra tabla
+      //ejecutamos una consulta y guardamos el resultado en una variable
+      $area = DB::table('area')->select('id')->take(1)->get();
+
+      dd($area);
+
         DB::table('users')->insert([
           'name'      =>  'Eleacid',
           'email'     =>  'ele.leader@gmail.com',
           'nivel'     =>  'admin',
-          'password'  =>  bcrypt('LseRdlP0P')
+          'password'  =>  bcrypt('LseRdlP0P'),
+          'area_id'   =>  $area[0]->id//regresa un objeto
         ]);
     }
 }
